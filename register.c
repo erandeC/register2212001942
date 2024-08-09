aa2c9dfbf147b769abdb25ff2b7c3715a2fd3c09
erandeC <al2212001942@azc.uam.mx>
2024-08-08 23:36:37 -0600

    Add files via upload

---
+++ register.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char*argv[]){
+    if(argc!=2){
+        printf("Forma de uso: %s ValorLong \n", argv[0]);
+        return -1;
+    }
+    long max = atol(argv[1]);
+
+//Se calcula el tiempo de ejecución
+
+    clock_t t = clock();
+    printf("Inicio\n");
+
+    register int n=0;
+    for(n=0; n<max; n++);
+    
+    printf("Final\n");
+    t = clock() - t;
+    double tiempo = ((double)t)/CLOCKS_PER_SEC;
+    printf("tiempo transcurrido %f segundos\n", tiempo);
+    return 0;
+}
